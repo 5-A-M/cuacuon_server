c303c542aa8bb5caf254c4ab0003d63abf3c3ce9
datispham <datistpham@gmail.com>
2022-08-10 15:06:48 +0700

    commit message

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ dotenv.config()
 const app= express()
 const server= http.createServer(app)
 
-app.use(cors({credentials: true}))
+app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 app.use(router)
