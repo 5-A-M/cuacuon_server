0dd8e09f6e321a73e52b24d34707856b31097fdd
datispham <datistpham@gmail.com>
2022-08-11 17:09:32 +0700

    commit message

--- route/user.js
+++ route/user.js
@@ -1,5 +1,7 @@
 import express from "express"
 import uploadCloud from "../cloudinary/upload.js"
+import { apiDetailAccessory } from "../controller/detail_accessory.js"
+import { apiDetailLuxury } from "../controller/detail_door_luxury.js"
 import { apiDetailProduct } from "../controller/detail_product.js"
 import { product_cuacuon } from "../controller/product_cuacuon.js"
 import { uploadimage } from "../controller/uploadimage.js"
@@ -12,6 +14,8 @@ router.post("/api/v2/upload", uploadCloud.single("file"), uploadimage)
 router.post("/api/v2/info/cuacuon", uploadInfo)
 router.post("/api/v2/info/phukien", uploadPhukien)
 router.get("/api/v1/get/cuacuon", product_cuacuon)
+router.get("/api/v1/get/detail/door_luxury", apiDetailLuxury)
 router.get("/api/v1/get/detail/door", apiDetailProduct)
+router.get("/api/v1/get/detail/accessory", apiDetailAccessory)
 
 export default router
