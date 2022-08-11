fa395ae5411c8402906bf124d3db1501c3cd143c
datispham <datistpham@gmail.com>
2022-08-11 17:42:08 +0700

    upload

--- controller/product_list.js
+++ controller/product_list.js
@@ -2,7 +2,7 @@ import { dbconnection } from "../database/init.js"
 // import cua from "../schema/cuacuon.js"
 
 export const product_list= (req, res)=> {
-    if(req.params.category=== "cua-thep-luxury") {
+    if(req.query.category=== "cua-thep-luxury") {
         dbconnection.collection("cua-luxury")
         .find({})
         .toArray((err, docs)=> {
@@ -10,7 +10,7 @@ export const product_list= (req, res)=> {
             return res.json(docs)
         })
     }
-    if(req.params.category=== "cua-thep-van-go") {
+    if(req.query.category=== "cua-thep-van-go") {
         dbconnection.collection("cua")
         .find({})
         .toArray((err, docs)=> {
@@ -18,7 +18,7 @@ export const product_list= (req, res)=> {
             return res.json(docs)
         })
     }
-    if(req.params.category=== "phu-kien-cua") {
+    if(req.query.category=== "phu-kien-cua") {
         dbconnection.collection("phukien")
         .find({})
         .toArray((err, docs)=> {
