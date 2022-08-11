8d0685e7c283f47d50dc5e8fae74feca341d2af6
datispham <datistpham@gmail.com>
2022-08-11 17:04:50 +0700

    upload sieucap vippro

--- controller/uploadInfo.js
+++ controller/uploadInfo.js
@@ -1,24 +1,50 @@
 import {dbconnection} from "../database/init.js"    
 
 export const uploadInfo= (req, res)=> {
-    dbconnection.collection("cua").insertOne({
-        name: req.body.name,
-        code: req.body.code, 
-        codeColor: req.body.codeColor,
-        openAir: req.body.openAir,
-        steelForWings: req.body.steelForWings,
-        steelForFrame: req.body.steelForFrame,
-        steelMaterial: req.body.steelMaterial,
-        surface: req.body.surface,
-        thickWings: req.body.thickWings,
-        glassDoor: req.body.glassDoor,
-        width: req.body.width,
-        height: req.body.height,
-        lock: req.body.lock,
-        hinge: req.body.hinge,
-        price: req.body.price,
-        photo: req.body.photo,
-    })    
-    .then(()=> res.json("success"))
-    .catch(err=> console.log(err))
+    if(parseInt(req.body.category) === 1) {
+
+        dbconnection.collection("cua").insertOne({
+            name: req.body.name,
+            code: req.body.code, 
+            codeColor: req.body.codeColor,
+            openAir: req.body.openAir,
+            steelForWings: req.body.steelForWings,
+            steelForFrame: req.body.steelForFrame,
+            steelMaterial: req.body.steelMaterial,
+            surface: req.body.surface,
+            thickWings: req.body.thickWings,
+            glassDoor: req.body.glassDoor,
+            width: req.body.width,
+            height: req.body.height,
+            lock: req.body.lock,
+            hinge: req.body.hinge,
+            price: req.body.price,
+            photo: req.body.photo,
+        })    
+        .then(()=> res.json("success"))
+        .catch(err=> console.log(err))
+    }
+    if(parseInt(req.body.category) === 2) {
+
+        dbconnection.collection("cua_luxury").insertOne({
+            name: req.body.name,
+            code: req.body.code, 
+            codeColor: req.body.codeColor,
+            openAir: req.body.openAir,
+            steelForWings: req.body.steelForWings,
+            steelForFrame: req.body.steelForFrame,
+            steelMaterial: req.body.steelMaterial,
+            surface: req.body.surface,
+            thickWings: req.body.thickWings,
+            glassDoor: req.body.glassDoor,
+            width: req.body.width,
+            height: req.body.height,
+            lock: req.body.lock,
+            hinge: req.body.hinge,
+            price: req.body.price,
+            photo: req.body.photo,
+        })    
+        .then(()=> res.json("success"))
+        .catch(err=> console.log(err))
+    }
 }
